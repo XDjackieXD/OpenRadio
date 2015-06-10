7ffa9a49bf450c8e80d0b41cbd616a77fc3eadb8
XDjackieXD <jakob.riepler@gmail.com>
2015-06-10 21:03:11 +0200

    Added the 3 lenses

--- src/main/java/at/chaosfield/openradio/common/item/LensItem.java
+++ src/main/java/at/chaosfield/openradio/common/item/LensItem.java
@@ -2,18 +2,51 @@ package at.chaosfield.openradio.common.item;
 
 import at.chaosfield.openradio.CreativeTab;
 import at.chaosfield.openradio.OpenRadio;
-import net.minecraft.creativetab.CreativeTabs;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
+import net.minecraft.util.MathHelper;
 
 /**
  * Created by Jakob Riepler (XDjackieXD)
  */
 public class LensItem extends Item{
 
-
     public LensItem(){
         maxStackSize = 1;
         setUnlocalizedName(OpenRadio.MODID + ":lens");
         setCreativeTab(CreativeTab.instance);
     }
-}
+
+
+    @SideOnly(Side.CLIENT)
+    private IIcon[] icons;
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void registerIcons(IIconRegister par1IconRegister)
+    {
+        icons = new IIcon[3];
+        icons[0] = par1IconRegister.registerIcon(OpenRadio.MODID + ":glasslens");
+        icons[1] = par1IconRegister.registerIcon(OpenRadio.MODID + ":quartzlens");
+        icons[2] = par1IconRegister.registerIcon(OpenRadio.MODID + ":diamondlens");
+    }
+
+    public static final String[] names = new String[] { "glasslens", "quartzlens", "diamondlens" };
+
+    @Override
+    public String getUnlocalizedName(ItemStack par1ItemStack)
+    {
+        int i = MathHelper.clamp_int(par1ItemStack.getItemDamage(), 0, 2);
+        return super.getUnlocalizedName() + "." + names[i];
+    }
+
+    @Override
+    public IIcon getIconFromDamage(int par1)
+    {
+        return icons[par1];
+    }
+  }
